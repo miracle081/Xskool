3193469ad687017933a346308af41b6eee61a70e
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-03-21 13:03:38 +0100

    first commit

---
+++ global/globalVariables.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+const AppContext = createContext();
+
+function AppProvider({ children }) {
+    const [doc, setDoc] = useState("");
+    const [docID, setDocID] = useState("");
+    const [userUID, setUserUID] = useState("F0oRUehjZwXPgpWgxrsI5HI8UBT2");
+    const [account, setAccount] = useState({});
+    const [preloader, setPreloader] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [userInfo, setUserInfo] = useState({ image: null, first_name: "", last_name: "", email: "" });
+
+
+    return (
+        <AppContext.Provider value={{
+            doc, setDoc,
+            posts, setPosts,
+            docID, setDocID,
+            account, setAccount,
+            userUID, setUserUID,
+            userInfo, setUserInfo,
+            preloader, setPreloader,
+
+            // Funtions
+            getUserInfo,
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export { AppContext, AppProvider }
